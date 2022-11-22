fcf54e2279b73791f03c395d32d3b43489e57d00
opilane <opilane@HP-CZC6297B93>
2022-11-22 14:01:22 +0200

    Add project files.

---
+++ Computer maintenance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Computer_maintenance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
